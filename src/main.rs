4e985aa00acc1d9ad23f20fb90ba16b1ba469511
carrotflakes <carrotflakes@gmail.com>
2020-02-15 03:53:54 +0900

    implement draw_line

--- src/main.rs
+++ src/main.rs
@@ -35,19 +35,20 @@ fn main() {
         }
     });
 
-    for y in 0..512 {
-        for (si, ps) in shapes.iter().enumerate() {
+    for (si, ps) in shapes.iter().enumerate() {
+        for y in 0..height {
             let mut vec = ps.windows(2).filter_map(|pair| intersection_(pair[0].0, pair[0].1, pair[1].0, pair[1].1, y as f64)).collect::<Vec<f64>>();
             vec.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
-            if !vec.is_empty() {
-                for i in 0..vec.len() / 2 {
-                    let s = vec[i * 2].max(0.0) as u32;
-                    let e = vec[i * 2 + 1].max(0.0).min((width - 1) as f64) as u32;
-                    for x in s..e {
-                        img.put_pixel(x, y, image::Rgb([[255, 128, 0], [0, 255, 128], [128, 0, 255]][si % 3]));
-                    }
+            for i in 0..vec.len() / 2 {
+                let s = vec[i * 2].max(0.0) as u32;
+                let e = vec[i * 2 + 1].max(0.0).min((width - 1) as f64) as u32;
+                for x in s..e {
+                    img.put_pixel(x, y, image::Rgb([[255, 128, 0], [0, 255, 128], [128, 0, 255]][si % 3]));
                 }
             }
+        }
+        for s in ps.windows(2) {
+            draw_line(&mut img, s[0], s[1], image::Rgb([[128, 64, 0], [0, 128, 64], [64, 0, 128]][si % 3]));
         }
     }
 
@@ -71,4 +72,28 @@ fn transform(p: &(f64, f64), translation: (f64, f64), rotation: f64, scale: (f64
     let (x, y) = (x * scale.0, y * scale.1);
     let (x, y) = (x + translation.0, y + translation.1);
     (x, y)
+}
+
+fn draw_line(img: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>, mut p1: (f64, f64), mut p2: (f64, f64), pixel: image::Rgb<u8>) {
+    if (p1.0 - p2.0).abs() < (p1.1 - p2.1).abs() {
+        if p1.1 > p2.1 {
+            std::mem::swap(&mut p1, &mut p2);
+        }
+        for y in p1.1.max(0.0) as u32..=p2.1.max(0.0).min(img.height() as f64 - 1.0) as u32 {
+            let x = (p2.0 - p1.0) * ((y as f64 - p1.1) / (p2.1 - p1.1)) + p1.0;
+            if 0.0 <= x && x < img.width() as f64 {
+                img.put_pixel(x as u32, y, pixel);
+            }
+        }
+    } else {
+        if p1.0 > p2.0 {
+            std::mem::swap(&mut p1, &mut p2);
+        }
+        for x in p1.0.max(0.0) as u32..=p2.0.max(0.0).min(img.width() as f64 - 1.0) as u32 {
+            let y = (p2.1 - p1.1) * ((x as f64 - p1.0) / (p2.0 - p1.0)) + p1.1;
+            if 0.0 <= y && y < img.height() as f64 {
+                img.put_pixel(x, y as u32, pixel);
+            }
+        }
+    }
 }
