8bf1fad06e076eae31c94ffa1bcb2ffb78907f24
carrotflakes <carrotflakes@gmail.com>
2020-08-11 05:38:26 +0900

    🥺

--- src/fill_path.rs
+++ src/fill_path.rs
@@ -1,13 +1,108 @@
 use crate::geometry;
-use crate::path2::PathEdge;
+use crate::path2::{PathEdge, Arc};
 use crate::point::Point;
 use crate::position_color::PositionColor;
 use image::{ImageBuffer, Pixel};
-use std::f64::{
-    consts::{FRAC_PI_2, PI},
-    NAN,
-};
+use std::f64::consts::{FRAC_PI_2, PI};
 
+#[derive(Debug, Clone)]
+enum Elm {
+    Line(Point, Point),
+    LeftArc {
+        arc: Arc,
+    },
+    RightArc {
+        arc: Arc,
+    },
+}
+
+#[derive(Debug, Clone)]
+struct ElmContainer {
+    bound: (f64, f64),
+    elm: Elm,
+    max: f64,
+}
+
+fn path_edges_to_elms(es: &Vec<PathEdge>) -> Vec<ElmContainer> {
+    let mut elms = Vec::new();
+    for e in es {
+        match e {
+            PathEdge::Line(p1, p2) => {
+                elms.push(ElmContainer {
+                    bound: (p1.1.min(p2.1), p1.1.max(p2.1)),
+                    elm: Elm::Line(*p1, *p2), max: 0.0
+                });
+            }
+            PathEdge::Arc(arc) => {
+                let (a1, a2) = angle_norm(arc.angle1, arc.angle2);
+                match (((a1 / FRAC_PI_2) as usize + 1) / 2, ((a2 / FRAC_PI_2) as usize + 1) / 2) {
+                    (0, 0) | (2, 2) => {
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 - a1.sin() * arc.radius),
+                            elm: Elm::RightArc{arc: arc.clone()}, max: 0.0
+                        });
+                    }
+                    (0, 1) | (2, 3) => {
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - arc.radius, arc.center.1 - a2.sin() * arc.radius),
+                            elm: Elm::LeftArc{arc: arc.clone()}, max: 0.0
+                        });
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - arc.radius, arc.center.1 - a1.sin() * arc.radius),
+                            elm: Elm::RightArc{arc: arc.clone()}, max: 0.0
+                        });
+                    }
+                    (0, 2) | (2, 4) => {
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - arc.radius, arc.center.1 + arc.radius),
+                            elm: Elm::LeftArc{arc: arc.clone()}, max: 0.0
+                        });
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - arc.radius, arc.center.1 - a1.sin() * arc.radius),
+                            elm: Elm::RightArc{arc: arc.clone()}, max: 0.0
+                        });
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 + arc.radius),
+                            elm: Elm::RightArc{arc: arc.clone()}, max: 0.0
+                        });
+                    }
+                    (1, 1) => {
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - a1.sin() * arc.radius, arc.center.1 - a2.sin() * arc.radius),
+                            elm: Elm::LeftArc{arc: arc.clone()}, max: 0.0
+                        });
+                    }
+                    (1, 2) => {
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - a1.sin() * arc.radius, arc.center.1 + arc.radius),
+                            elm: Elm::LeftArc{arc: arc.clone()}, max: 0.0
+                        });
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 + arc.radius),
+                            elm: Elm::RightArc{arc: arc.clone()}, max: 0.0
+                        });
+                    }
+                    (1, 3) => {
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - arc.radius, arc.center.1 - a1.sin() * arc.radius),
+                            elm: Elm::LeftArc{arc: arc.clone()}, max: 0.0
+                        });
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 + arc.radius),
+                            elm: Elm::LeftArc{arc: arc.clone()}, max: 0.0
+                        });
+                        elms.push(ElmContainer {
+                            bound: (arc.center.1 - arc.radius, arc.center.1 + arc.radius),
+                            elm: Elm::RightArc{arc: arc.clone()}, max: 0.0
+                        });
+                    }
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
+    elms.into_iter().filter(|e| e.bound.0 < e.bound.1).collect()
+}
 
 pub fn draw_fill<X, C: PositionColor<X>>(
     img: &mut ImageBuffer<X, Vec<u8>>,
@@ -16,25 +111,26 @@ pub fn draw_fill<X, C: PositionColor<X>>(
 ) where
     X: Pixel<Subpixel = u8> + 'static,
 {
+    let width = img.width() as usize;
+    let mut ecs = path_edges_to_elms(edges);
+    for ec in ecs.iter_mut() {
+        ec.max = ec.area(img.height() as f64, img.width() as f64);
+    }
+    let ecslen = ecs.len();
+    let ww = width + 2;
+    let mut acc = vec![0.0; ecslen * ww];
     for y in 0..img.height() {
-        let mut accs: Vec<_> = edges.iter().map(|e| {
-            match e {
-                PathEdge::Line(p1, p2) => {
-                    area(*p1, *p2, y as f64, 0.0)
-                }
-                PathEdge::Arc(arc) => arc_area(arc.center, arc.radius, arc.angle1, arc.angle2, y as f64, 0.0),
-            }
-        }).collect();
-        for x in 0..img.width() {
+        for i in 0..ecslen {
+            acc[i * ww + (-(y as i32)).rem_euclid(ww as i32) as usize] = 0.0;
+        }
+        for x in 0..img.width() as usize {
             let mut r = 0.0;
-            for (i, e) in edges.iter().enumerate() {
-                let a = match e {
-                    PathEdge::Line(p1, p2) =>
-                        area(*p1, *p2, y as f64, x as f64 + 1.0),
-                    PathEdge::Arc(arc) => arc_area(arc.center, arc.radius, arc.angle1, arc.angle2, y as f64, x as f64 + 1.0),
-                };
-                r += a - accs[i];
-                accs[i] = a;
+            for (i, e) in ecs.iter().enumerate() {
+                let offset = i * ww;
+                let a = e.area((y + 1) as f64, (x + 1) as f64);
+                r += a + acc[offset + (x as i32 + 1 - y as i32).rem_euclid(ww as i32) as usize] - acc[offset + (x as i32 + 2 - y as i32).rem_euclid(ww as i32) as usize] - acc[offset + (x as i32 - y as i32).rem_euclid(ww as i32) as usize];
+                acc[offset + (x as i32 + 1 - y as i32).rem_euclid(ww as i32) as usize] = a;
+                //r += e.area((y + 1) as f64, (x + 1) as f64) - e.area((y + 1) as f64, x as f64) - e.area(y as f64, (x + 1) as f64) + e.area(y as f64, x as f64)
             }
             img_blend_pixel(img, position_color, x as i32, y as i32, r);//.min(1.0).max(0.0)
             //img_blend_pixel(img, position_color, x as i32, y as i32, r.min(1.0).max(0.0));
@@ -42,132 +138,47 @@ pub fn draw_fill<X, C: PositionColor<X>>(
     }
 }
 
-fn scan(edges: &Vec<PathEdge>, y: f64) -> Vec<f64> {
-    let mut vec = Vec::new();
-    for edge in edges {
-        match edge {
-            PathEdge::Line(p1, p2) => {
-                vec.push(
-                    geometry::intersect_segment_and_horizon(p1.0, p1.1, p2.0, p2.1, y)
-                        .unwrap_or(NAN),
-                );
+impl ElmContainer {
+    fn area(&self, y: f64, x: f64) -> f64 {
+        if y <= self.bound.0 {
+            return 0.0;
+        }
+        let y = y.min(self.bound.1);
+        match self.elm {
+            Elm::Line(p1, p2) => {
+                segment_area(p1, p2, y, x)
             }
-            PathEdge::Arc(_arc) => {
-                // ittann mushi
-                vec.push(NAN);
+            Elm::LeftArc { ref arc } => {
+                let h = y - self.bound.0;
+                (arc.angle1 - arc.angle2).signum() *
+                if x < arc.center.0 - arc.radius {
+                    x * h
+                } else {
+                    let x = x.min(arc.center.0);
+                    x * h - circle_area(arc.center, arc.radius, y, x)
+                }
+            }
+            Elm::RightArc { ref arc } => {
+                let h = y - self.bound.0;
+                (arc.angle2 - arc.angle1).signum() *
+                if x < arc.center.0 {
+                    x * h
+                } else if x < arc.center.0 + arc.radius {
+                    arc.center.0 * h + circle_area(arc.center, arc.radius, y, x) - circle_area(arc.center, arc.radius, y, arc.center.0)
+                } else {
+                    arc.center.0 * h + circle_area(arc.center, arc.radius, y, arc.center.0)
+                }
             }
         }
     }
-    vec
 }
 
-fn arc_area(center: Point, radius: f64, angle1: f64, angle2: f64, y: f64, x: f64) -> f64 {
-    let (a1, a2) = angle_norm(angle1, angle2);
-    let upper = y.max(center.1 - radius);
-    let lower = (y + 1.0).min(center.1 + radius);
-    if upper >= lower {
-        return 0.0;
-    }
-    ({ // right
-        match
-            match (((a1 / FRAC_PI_2) as usize + 1) / 2, ((a2 / FRAC_PI_2) as usize + 1) / 2) {
-                (0, 0) | (2, 2) => Some((-a2.sin(), -a1.sin(), false)),
-                (0, 1) | (2, 3) => Some((-1.0, -a1.sin(), false)),
-                (0, 2) | (2, 4) => Some((-a2.sin(), -a1.sin(), true)),
-                (1, 1) => None,
-                (1, 2) => Some((-a2.sin(), 1.0, false)),
-                (1, 3) => Some((-1.0, 1.0, false)),
-                _ => unreachable!(),
-            } {
-                Some((y1, y2, false)) => {
-                    let u = upper.max(center.1 + y1 * radius);
-                    let l = lower.min(center.1 + y2 * radius);
-                    if u >= l { 0.0 } else {
-                        right_arc_area(center, radius, u, l, x)
-                    }
-                }
-                Some((y1, y2, true)) => {
-                    0.0 + {
-                        let u = upper.max(center.1 - radius);
-                        let l = lower.min(center.1 + y2 * radius);
-                        if u >= l { 0.0 } else {
-                            right_arc_area(center, radius, u, l, x)
-                        }
-                    } + {
-                        let u = upper.max(center.1 + y1 * radius);
-                        let l = lower.min(center.1 + radius);
-                        if u >= l { 0.0 } else {
-                            right_arc_area(center, radius, u, l, x)
-                        }
-                    }
-                }
-                None => 0.0
-            }
-        }
-    - { // left
-        match
-            match (((a1 / FRAC_PI_2) as usize + 1) / 2, ((a2 / FRAC_PI_2) as usize + 1) / 2) {
-                (0, 0) | (2, 2) => None,
-                (0, 1) | (2, 3) => Some((-1.0, -a2.sin(), false)),
-                (0, 2) | (2, 4) => Some((-1.0, 1.0, false)),
-                (1, 1) => Some((-a1.sin(), -a2.sin(), false)),
-                (1, 2) => Some((-a1.sin(), 1.0, false)),
-                (1, 3) => Some((-a2.sin(), -a1.sin(), true)),
-                _ => unreachable!(),
-            } {
-                Some((y1, y2, false)) => {
-                    let u = upper.max(center.1 + y1 * radius);
-                    let l = lower.min(center.1 + y2 * radius);
-                    if u >= l { 0.0 } else {
-                    left_arc_area(center, radius, u, l, x)
-                    }
-                }
-                Some((y1, y2, true)) => {
-                    0.0 + {
-                        let u = upper.max(center.1 - radius);
-                        let l = lower.min(center.1 + y1 * radius);
-                        if u >= l { 0.0 } else {
-                            left_arc_area(center, radius, u, l, x)
-                        }
-                    } + {
-                        let u = upper.max(center.1 + y2 * radius);
-                        let l = lower.min(center.1 + radius);
-                        if u >= l { 0.0 } else {
-                            left_arc_area(center, radius, u, l, x)
-                        }
-                    }
-                }
-                None => 0.0
-            }
-    }) * (angle2 - angle1).signum()
-}
-
-fn left_arc_area(center: Point, radius: f64, upper: f64, lower: f64, x: f64) -> f64 {
-    if x < center.0 - radius {
-        x * (lower - upper)
-    } else if x < center.0 {
-        x * (lower - upper) - ((cliped_arc_area(center, radius, upper, x) - cliped_arc_area(center, radius, lower, x)))
-    } else {
-        center.0 * (lower - upper) - (cliped_arc_area(center, radius, upper, center.0) - cliped_arc_area(center, radius, lower, center.0))
-    }
-}
-
-fn right_arc_area(center: Point, radius: f64, upper: f64, lower: f64, x: f64) -> f64 {
-    if x < center.0 {
-        x * (lower - upper)
-    } else if x < center.0 + radius {
-        center.0 * (lower - upper) + (cliped_arc_area(center, radius, upper, x) - cliped_arc_area(center, radius, lower, x)) - (cliped_arc_area(center, radius, upper, center.0) - cliped_arc_area(center, radius, lower, center.0))
-    } else {
-        center.0 * (lower - upper) + (cliped_arc_area(center, radius, upper, center.0) - cliped_arc_area(center, radius, lower, center.0))
-    }
-}
-
-fn cliped_arc_area(center: Point, radius: f64, upper: f64, right: f64) -> f64 {
+fn circle_area(center: Point, radius: f64, lower: f64, right: f64) -> f64 {
     fn f(d: f64) -> f64 {
         d.acos() - (1.0 - d * d).sqrt() * d
     }
     let w = (center.0 - right) / radius;
-    let h = (upper - center.1) / radius;
+    let h = (center.1 - lower) / radius;
 
     radius.powi(2) *
     if w.powi(2) + h.powi(2) >= 1.0 {
@@ -182,17 +193,17 @@ fn cliped_arc_area(center: Point, radius: f64, upper: f64, right: f64) -> f64 {
     }
 }
 
-fn area(p1: Point, p2: Point, y: f64, x: f64) -> f64 {
-    let y1 = p1.1.min(p2.1).max(y);
-    let y2 = p1.1.max(p2.1).min(y + 1.0);
+fn segment_area(p1: Point, p2: Point, lower: f64, right: f64) -> f64 {
+    let y1 = p1.1.min(p2.1);
+    let y2 = p1.1.max(p2.1).min(lower);
     if y1 < y2 {
         let x1 = geometry::intersect_line_and_horizon(p1, p2, y1);
         let x2 = geometry::intersect_line_and_horizon(p1, p2, y2);
         let (x1, x2) = if x1 < x2 {(x1, x2)} else {(x2, x1)};
-        ((y2 - y1) * if x < x1 {
-            x
-        } else if x < x2 {
-            x - (x - x1).powi(2) / (x2 - x1) / 2.0
+        ((y2 - y1) * if right < x1 {
+            right
+        } else if right < x2 {
+            right - (right - x1).powi(2) / (x2 - x1) / 2.0
         } else {
             (x1 + x2) / 2.0
         }).copysign(p1.1 - p2.1)
