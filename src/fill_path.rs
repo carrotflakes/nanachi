6da8ce1516535fee3a41e1ba9ddb22b5dc3299ae
carrotflakes <carrotflakes@gmail.com>
2020-08-13 01:05:51 +0900

    give ElmContainer signum

--- src/fill_path.rs
+++ src/fill_path.rs
@@ -20,6 +20,7 @@ enum Elm {
 struct ElmContainer {
     bound: (f64, f64),
     elm: Elm,
+    signum: f64,
 }
 
 fn path_edges_to_elms(es: &Vec<PathEdge>) -> Vec<ElmContainer> {
@@ -29,7 +30,7 @@ fn path_edges_to_elms(es: &Vec<PathEdge>) -> Vec<ElmContainer> {
             PathEdge::Line(p1, p2) => {
                 elms.push(ElmContainer {
                     bound: (p1.1.min(p2.1), p1.1.max(p2.1)),
-                    elm: Elm::Line(*p1, *p2)
+                    elm: Elm::Line(*p1, *p2), signum: (p1.1 - p2.1).signum()
                 });
             }
             PathEdge::Arc(arc) => {
@@ -38,61 +39,61 @@ fn path_edges_to_elms(es: &Vec<PathEdge>) -> Vec<ElmContainer> {
                     (0, 0) | (2, 2) => {
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 - a1.sin() * arc.radius),
-                            elm: Elm::RightArc{arc: arc.clone()}
+                            elm: Elm::RightArc{arc: arc.clone()}, signum: (arc.angle2 - arc.angle1).signum()
                         });
                     }
                     (0, 1) | (2, 3) => {
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - arc.radius, arc.center.1 - a2.sin() * arc.radius),
-                            elm: Elm::LeftArc{arc: arc.clone()}
+                            elm: Elm::LeftArc{arc: arc.clone()}, signum: (arc.angle1 - arc.angle2).signum()
                         });
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - arc.radius, arc.center.1 - a1.sin() * arc.radius),
-                            elm: Elm::RightArc{arc: arc.clone()}
+                            elm: Elm::RightArc{arc: arc.clone()}, signum: (arc.angle2 - arc.angle1).signum()
                         });
                     }
                     (0, 2) | (2, 4) => {
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - arc.radius, arc.center.1 + arc.radius),
-                            elm: Elm::LeftArc{arc: arc.clone()}
+                            elm: Elm::LeftArc{arc: arc.clone()}, signum: (arc.angle1 - arc.angle2).signum()
                         });
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - arc.radius, arc.center.1 - a1.sin() * arc.radius),
-                            elm: Elm::RightArc{arc: arc.clone()}
+                            elm: Elm::RightArc{arc: arc.clone()}, signum: (arc.angle2 - arc.angle1).signum()
                         });
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 + arc.radius),
-                            elm: Elm::RightArc{arc: arc.clone()}
+                            elm: Elm::RightArc{arc: arc.clone()}, signum: (arc.angle2 - arc.angle1).signum()
                         });
                     }
                     (1, 1) => {
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - a1.sin() * arc.radius, arc.center.1 - a2.sin() * arc.radius),
-                            elm: Elm::LeftArc{arc: arc.clone()}
+                            elm: Elm::LeftArc{arc: arc.clone()}, signum: (arc.angle1 - arc.angle2).signum()
                         });
                     }
                     (1, 2) => {
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - a1.sin() * arc.radius, arc.center.1 + arc.radius),
-                            elm: Elm::LeftArc{arc: arc.clone()}
+                            elm: Elm::LeftArc{arc: arc.clone()}, signum: (arc.angle1 - arc.angle2).signum()
                         });
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 + arc.radius),
-                            elm: Elm::RightArc{arc: arc.clone()}
+                            elm: Elm::RightArc{arc: arc.clone()}, signum: (arc.angle2 - arc.angle1).signum()
                         });
                     }
                     (1, 3) => {
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - arc.radius, arc.center.1 - a1.sin() * arc.radius),
-                            elm: Elm::LeftArc{arc: arc.clone()}
+                            elm: Elm::LeftArc{arc: arc.clone()}, signum: (arc.angle1 - arc.angle2).signum()
                         });
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - a2.sin() * arc.radius, arc.center.1 + arc.radius),
-                            elm: Elm::LeftArc{arc: arc.clone()}
+                            elm: Elm::LeftArc{arc: arc.clone()}, signum: (arc.angle1 - arc.angle2).signum()
                         });
                         elms.push(ElmContainer {
                             bound: (arc.center.1 - arc.radius, arc.center.1 + arc.radius),
-                            elm: Elm::RightArc{arc: arc.clone()}
+                            elm: Elm::RightArc{arc: arc.clone()}, signum: (arc.angle2 - arc.angle1).signum()
                         });
                     }
                     _ => unreachable!(),
@@ -135,13 +136,13 @@ impl ElmContainer {
             return 0.0;
         }
         let y = y.min(self.bound.1);
+        self.signum *
         match self.elm {
             Elm::Line(p1, p2) => {
                 segment_area(p1, p2, y, x)
             }
             Elm::LeftArc { ref arc } => {
                 let h = y - self.bound.0;
-                (arc.angle1 - arc.angle2).signum() *
                 if x < arc.center.0 - arc.radius {
                     x * h
                 } else {
@@ -151,7 +152,6 @@ impl ElmContainer {
             }
             Elm::RightArc { ref arc } => {
                 let h = y - self.bound.0;
-                (arc.angle2 - arc.angle1).signum() *
                 if x < arc.center.0 {
                     x * h
                 } else if x < arc.center.0 + arc.radius {
@@ -191,13 +191,13 @@ fn segment_area(p1: Point, p2: Point, lower: f64, right: f64) -> f64 {
         let x1 = geometry::intersect_line_and_horizon(p1, p2, y1);
         let x2 = geometry::intersect_line_and_horizon(p1, p2, y2);
         let (x1, x2) = if x1 < x2 {(x1, x2)} else {(x2, x1)};
-        ((y2 - y1) * if right < x1 {
+        (y2 - y1) * if right < x1 {
             right
         } else if right < x2 {
             right - (right - x1).powi(2) / (x2 - x1) / 2.0
         } else {
             (x1 + x2) / 2.0
-        }).copysign(p1.1 - p2.1)
+        }
     } else {
         0.0
     }
