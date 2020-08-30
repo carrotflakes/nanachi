dd6852b189fcfdb651047d9a097a30565e5cf808
carrotflakes <carrotflakes@gmail.com>
2020-08-30 17:22:11 +0900

    add Ellipse::normalize_half_pi

--- src/models.rs
+++ src/models.rs
@@ -32,7 +32,33 @@ pub struct Ellipse {
 
 impl Ellipse {
     pub fn normalize(&self) -> Ellipse {
-        dbg!(self);
+        // rotationを0degから180degに正規化
+        let mut rotation = self.rotation.rem_euclid(PI * 2.0);
+        let mut angle1 = self.angle1;
+        let mut angle2 = self.angle2;
+        if PI < rotation {
+            angle1 -= PI;
+            angle2 -= PI;
+            rotation -= PI;
+        }
+        if angle1 < angle2 {
+            angle2 = angle2 + angle1.rem_euclid(PI * 2.0) - angle1;
+            angle1 = angle1.rem_euclid(PI * 2.0);
+        } else {
+            angle1 = angle1 + angle2.rem_euclid(PI * 2.0) - angle2;
+            angle2 = angle2.rem_euclid(PI * 2.0);
+        }
+        Ellipse {
+            center: self.center,
+            radius_x: self.radius_x,
+            radius_y: self.radius_y,
+            rotation,
+            angle1,
+            angle2,
+        }
+    }
+
+    pub fn normalize_half_pi(&self) -> Ellipse {
         // rotationを0degから90degに正規化
         let mut rotation = self.rotation.rem_euclid(PI * 2.0);
         let mut radius_x = self.radius_x;
@@ -48,8 +74,8 @@ impl Ellipse {
             rotation -= FRAC_PI_2;
             radius_x = self.radius_y;
             radius_y = self.radius_x;
-            angle1 += FRAC_PI_2;
-            angle2 += FRAC_PI_2;
+            angle1 += FRAC_PI_2; // TODO
+            angle2 += FRAC_PI_2; // TODO
         }
         if angle1 < angle2 {
             angle2 = angle2 + angle1.rem_euclid(PI * 2.0) - angle1;
@@ -58,14 +84,14 @@ impl Ellipse {
             angle1 = angle1 + angle2.rem_euclid(PI * 2.0) - angle2;
             angle2 = angle2.rem_euclid(PI * 2.0);
         }
-        dbg!(Ellipse {
+        Ellipse {
             center: self.center,
             radius_x,
             radius_y,
             rotation,
             angle1,
             angle2,
-        })
+        }
     }
 
     pub fn bound(&self) -> (f64, f64, f64, f64) {
