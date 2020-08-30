3f9d1b1884365e4dffb021a856c667cf4a925de0
carrotflakes <carrotflakes@gmail.com>
2020-08-30 21:28:23 +0900

    path_builder takes x and y (not Point)

--- src/path_builder.rs
+++ src/path_builder.rs
@@ -18,7 +18,8 @@ impl PathBuilder {
         }
     }
 
-    pub fn move_to(mut self, p: Point) -> PathBuilder {
+    pub fn move_to(mut self, x: f64, y: f64) -> PathBuilder {
+        let p = Point(x, y);
         if !self.items.is_empty() {
             self.items.push(PathItem::Line(Line(self.current_pos, p)));
         }
@@ -26,7 +27,8 @@ impl PathBuilder {
         self
     }
 
-    pub fn line_to(mut self, p: Point) -> PathBuilder {
+    pub fn line_to(mut self, x: f64, y: f64) -> PathBuilder {
+        let p = Point(x, y);
         if self.current_pos != p {
             self.items.push(PathItem::Line(Line(self.current_pos, p)));
             self.current_pos = p;
@@ -34,7 +36,8 @@ impl PathBuilder {
         self
     }
 
-    pub fn arc(mut self, center: Point, radius: f64, angle1: f64, angle2: f64) -> PathBuilder {
+    pub fn arc(mut self, x: f64, y: f64, radius: f64, angle1: f64, angle2: f64) -> PathBuilder {
+        let center = Point(x, y);
         let arc = PathItem::Arc(Arc{
             center, radius, angle1, angle2
         });
@@ -46,7 +49,8 @@ impl PathBuilder {
         self
     }
 
-    pub fn ellipse(mut self, center: Point, radius_x: f64, radius_y: f64, rotation: f64, angle1: f64, angle2: f64) -> PathBuilder {
+    pub fn ellipse(mut self, x: f64, y: f64, radius_x: f64, radius_y: f64, rotation: f64, angle1: f64, angle2: f64) -> PathBuilder {
+        let center = Point(x, y);
         let ellipse = PathItem::Ellipse(Ellipse{
             center, radius_x, radius_y, rotation, angle1, angle2
         });
@@ -58,11 +62,11 @@ impl PathBuilder {
         self
     }
 
-    pub fn quad(mut self, control: Point, point: Point) -> PathBuilder {
+    pub fn quad(mut self, control_x: f64, control_y: f64, x: f64, y: f64) -> PathBuilder {
         let quad = PathItem::Quad(Quad{
             start: self.current_pos,
-            end: point,
-            control1: control,
+            end: Point(x, y),
+            control1: Point(control_x, control_y),
         });
         if self.current_pos != quad.left_point() {
             self.items.push(PathItem::Line(Line(self.current_pos, quad.left_point())));
@@ -74,7 +78,7 @@ impl PathBuilder {
 
     pub fn close(self) -> PathBuilder {
         let p = self.items[0].left_point();
-        self.line_to(p)
+        self.line_to(p.0, p.1)
     }
 
     pub fn end(self) -> Path {
