a1b92420209aa548734ebf8c4ff9b3ce0e5d5a40
carrotflakes <carrotflakes@gmail.com>
2020-09-04 00:40:57 +0900

    implement Join::Miter (WIP)

--- src/path_outline.rs
+++ src/path_outline.rs
@@ -20,6 +20,7 @@ pub enum Cap {
 }
 
 pub fn path_outline(path: &Path, width: f64, join: &Join, cap: &Cap) -> Vec<PathItem> {
+    assert_ne!(width, 0.0);
     let mut pis = Vec::with_capacity(path.0.len() * 4);
     path_item_bold(&mut pis, &path.0[0], width);
     let mut m = pis.len();
@@ -31,8 +32,8 @@ pub fn path_outline(path: &Path, width: f64, join: &Join, cap: &Cap) -> Vec<Path
         let p21 = pis[l + 1].right_point();
         let p22 = pis[m - 1].left_point();
         let p0 = pi.left_point();
+        m = pis.len();
         add_join(&mut pis, join, p0, p11, p12, p21, p22);
-        m = pis.len() - 2;
     }
     if path.is_closed() {
         let p11 = pis[m - 2].right_point();
@@ -56,11 +57,11 @@ fn add_join(pis: &mut Vec<PathItem>, join: &Join, center: Point, start1: Point, 
     match join {
         Join::Round => {
             if geometry::point_is_right_side_of_line(start1 - center, end1 - center) {
-                pis.push(PathItem::Arc(Arc::from_points(center, start1, end1)));
-                pis.push(PathItem::Line(Line(start2, end2)));
-            } else {
                 pis.push(PathItem::Line(Line(start1, end1)));
                 pis.push(PathItem::Arc(Arc::from_points(center, start2, end2)));
+            } else {
+                pis.push(PathItem::Arc(Arc::from_points(center, start1, end1)));
+                pis.push(PathItem::Line(Line(start2, end2)));
             }
         }
         Join::Bevel => {
@@ -68,7 +69,27 @@ fn add_join(pis: &mut Vec<PathItem>, join: &Join, center: Point, start1: Point, 
             pis.push(PathItem::Line(Line(start2, end2)));
         }
         Join::Miter(_) => {
-            todo!()
+            if geometry::point_is_right_side_of_line(start1 - center, end1 - center) {
+                let p = geometry::intersect_line_and_line(
+                    start2,
+                    start2 + Point(start2.1 - center.1, center.0 - start2.0),
+                    end2,
+                    end2 + Point(center.1 - end2.1, end2.0 - center.0),
+                );
+                pis.push(PathItem::Line(Line(start1, end1)));
+                pis.push(PathItem::Line(Line(start2, p)));
+                pis.push(PathItem::Line(Line(p, end2)));
+            } else {
+                let p = geometry::intersect_line_and_line(
+                    start1,
+                    start1 + Point(start1.1 - center.1, center.0 - start1.0),
+                    end1,
+                    end1 + Point(center.1 - end1.1, end1.0 - center.0),
+                );
+                pis.push(PathItem::Line(Line(start1, p)));
+                pis.push(PathItem::Line(Line(p, end1)));
+                pis.push(PathItem::Line(Line(start2, end2)));
+            }
         }
         Join::NoJoin => {
             pis.push(PathItem::Line(Line(start1, end2)));
@@ -86,8 +107,7 @@ fn add_cap(pis: &mut Vec<PathItem>, cap: &Cap, start: Point, end: Point) {
             pis.push(PathItem::Line(Line(start, end)));
         }
         Cap::Square => {
-            let v = end - start;
-            let v = Point(v.1, -v.0) * 0.5;
+            let v = Point(end.1 - start.1, start.0 - end.0) * 0.5;
             pis.push(PathItem::Line(Line(start, start + v)));
             pis.push(PathItem::Line(Line(start + v, end + v)));
             pis.push(PathItem::Line(Line(end + v, end)));
