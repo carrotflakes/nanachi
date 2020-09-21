1508fd85d4d3b17253f845f2b1f9304c57f6b63a
carrotflakes <carrotflakes@gmail.com>
2020-09-22 00:32:07 +0900

    add Perf compositor for performance baseline

--- src/compositor/mod.rs
+++ src/compositor/mod.rs
@@ -4,3 +4,21 @@ pub mod basic_enum;
 pub trait Compositor<T> {
     fn composite(&self, dst: &T, src: &T, alpha: f64) -> T;
 }
+
+use image::{Rgb, Rgba};
+
+pub struct Perf;
+
+impl Compositor<Rgba<u8>> for Perf {
+    #[allow(unused_variables)]
+    fn composite(&self, a: &Rgba<u8>, b: &Rgba<u8>, alpha: f64) -> Rgba<u8> {
+        Rgba([a.0[0], b.0[0], alpha as u8, 255])
+    }
+}
+
+impl Compositor<Rgb<u8>> for Perf {
+    #[allow(unused_variables)]
+    fn composite(&self, a: &Rgb<u8>, b: &Rgb<u8>, alpha: f64) -> Rgb<u8> {
+        Rgb([a.0[0], b.0[0], alpha as u8])
+    }
+}
