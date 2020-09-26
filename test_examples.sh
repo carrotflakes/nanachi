34c5501e4ea85aecf5e42a926cfd180b1683ea73
carrotflakes <carrotflakes@gmail.com>
2020-09-26 20:04:45 +0900

    make path_data_notaion feature enabled as default

--- test_examples.sh
+++ test_examples.sh
@@ -6,7 +6,7 @@ cargo run --release --example path3
 cargo run --release --example k_curve
 cargo run --release --example nanachi
 cargo run --release --example context
-cargo run --release --example path_data_notation --features "path-data-notation"
-cargo run --release --example new_nanachi --features "path-data-notation"
+cargo run --release --example path_data_notation
+cargo run --release --example new_nanachi
 
 echo "All tests passed! 🍰😃"
