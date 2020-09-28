ba1ed87d40cfa80868333f671ea12fda5ef1e139
carrotflakes <carrotflakes@gmail.com>
2020-09-29 02:12:33 +0900

    update gui

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -3,8 +3,10 @@ use nanachi::{
     compositor,
     context::{Context, FillStyle},
     fill_color, fill_rule,
-    image::Rgba,
-    image_crate_adapter::buffer_rgba_f32_to_rgba_image,
+    image::RgbaImage,
+    pixel::Rgba,
+    path_builder::PathBuilder,
+    matrix::Matrix2d,
 };
 use std::thread;
 use std::time::Duration;
@@ -12,57 +14,67 @@ use std::time::Duration;
 fn main() {
     gui::mount_gui(&|render, event_pump| {
         let (width, height) = (640usize, 480usize);
-        let mut buffer = nanachi::image::ImageBuffer::from_pixel(
-            width as u32,
-            height as u32,
-            Rgba([1.0f32, 1.0, 1.0, 1.0]),
-        );
-        let mut context = Context::from_image(&mut buffer);
+        let mut context =
+            Context::from_pixel(width as u32, height as u32, Rgba([1.0f32, 1.0, 1.0, 1.0])).high_quality();
 
-        let color = nanachi::fill_color::Solid::new(Rgba([0.1f32, 0.1, 0.1, 1.0]));
+        let color1 = nanachi::fill_color::Solid::new(Rgba([0.1f32, 0.1, 0.1, 1.0]));
         let color2 = nanachi::fill_color::Solid::new(Rgba([0.9f32, 0.0, 0.0, 1.0]));
+        let fill_style1 = FillStyle {
+            color: color1,
+            compositor: compositor::SrcOver,
+            fill_rule: fill_rule::NonZero,
+            pixel: Default::default(),
+        };
+        let fill_style2 = FillStyle {
+            color: color2,
+            compositor: compositor::SrcOver,
+            fill_rule: fill_rule::NonZero,
+            pixel: Default::default(),
+        };
 
         let mut points: Vec<(f64, f64)> = vec![];
 
+        let circle = {
+            let mut pb = PathBuilder::new();
+            pb.arc(0.0, 0.0, 10.0, 0.0, 360.0f64.to_radians());
+            pb.end()
+        };
+        let circle_style = FillStyle {
+            color: nanachi::fill_color::Solid::new(Rgba([1.0f32, 0.0, 0.0, 0.7])),
+            compositor: compositor::SrcOver,
+            fill_rule: fill_rule::NonZero,
+            pixel: Default::default(),
+        };
+
+        let mut count = 0;
+
         'running: loop {
-            render(&buffer_rgba_f32_to_rgba_image(&context.image.clone()));
+            context.clear(&fill_color::Solid::new(Rgba([1.0, 1.0, 1.0, 1.0])));
+            context.transformed_context(&Matrix2d::new()
+                .scale(((count as f64) * 10.0).to_radians().sin(), 1.0)
+                .translate(12.0, 12.0),
+            ).fill(&circle, &circle_style);
+            count += 1;
+            if 2 <= points.len() {
+                let path = nanachi::path::Path::from_points(
+                    &points.iter().map(|x| (*x).into()).collect(),
+                );
+                context.stroke(&path, &fill_style1, 2.0);
+            }
+            if 2 <= points.len() {
+                let path = points.iter().map(|x| (*x).into()).collect();
+                let path = nanachi::path::Path::from_bezier2_points(
+                    &nanachi::k_curve::k_curve(path, false, 4),
+                );
+                context.stroke(&path, &fill_style2, 2.0);
+            }
+            let img: RgbaImage = (&context.image).into();
+            render(&img);
             for ev in event_pump.poll_iter() {
                 println!("{:?}", ev);
                 match ev {
                     gui::Event::MouseButtonDown { x, y, .. } => {
-                        context.clear(&fill_color::Solid::new(Rgba([1.0, 1.0, 1.0, 1.0])));
                         points.push((x as f64, y as f64));
-                        if 2 <= points.len() {
-                            let path = nanachi::path::Path::from_points(
-                                &points.iter().map(|x| (*x).into()).collect(),
-                            );
-                            context.stroke(
-                                &path,
-                                &FillStyle {
-                                    color: color.clone(),
-                                    compositor: compositor::SrcOver,
-                                    fill_rule: fill_rule::NonZero,
-                                    pixel: Default::default(),
-                                },
-                                2.0,
-                            );
-                        }
-                        if 2 <= points.len() {
-                            let path = points.iter().map(|x| (*x).into()).collect();
-                            let path = nanachi::path::Path::from_bezier2_points(
-                                &nanachi::k_curve::k_curve(path, false, 4),
-                            );
-                            context.stroke(
-                                &path,
-                                &FillStyle {
-                                    color: color2.clone(),
-                                    compositor: compositor::SrcOver,
-                                    fill_rule: fill_rule::NonZero,
-                                    pixel: Default::default(),
-                                },
-                                2.0,
-                            );
-                        }
                     }
                     gui::Event::Quit { .. }
                     | gui::Event::KeyDown {
