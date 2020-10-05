347657b51790b3945671d2611b1d4fac4a8c6754
carrotflakes <carrotflakes@gmail.com>
2020-10-06 03:33:07 +0900

    fix rect and remove square

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,16 +1,12 @@
 use crate::{path::Path, point::Point};
 
-pub fn square(x: f64, y: f64, size: f64) -> Path {
-    rect(x, y, size, size)
-}
-
 pub fn rect(x: f64, y: f64, width: f64, height: f64) -> Path {
     Path::from_points(&vec![
-        Point(x - width, y - height),
-        Point(x - width, y + height),
+        Point(x, y),
+        Point(x, y + height),
         Point(x + width, y + height),
-        Point(x + width, y - height),
-        Point(x - width, y - height),
+        Point(x + width, y),
+        Point(x, y),
     ], true)
 }
 
