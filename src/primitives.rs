ec6fb9c1de52c35fe6d72a36a82698d95850fe2b
carrotflakes <carrotflakes@gmail.com>
2020-10-07 01:57:56 +0900

    add circle function in primitives.rs

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,13 +1,33 @@
-use crate::{path::Path, point::Point};
+use crate::{
+    models::Arc,
+    path::{Path, PathItem},
+    point::Point,
+};
+use std::f64::consts::PI;
+
+pub fn circle(x: f64, y: f64, radius: f64) -> Path {
+    Path::new(vec![
+        PathItem::Arc(Arc {
+            center: Point(x, y),
+            radius,
+            angle1: 0.0,
+            angle2: PI * 2.0,
+        }),
+        PathItem::CloseAndJump,
+    ])
+}
 
 pub fn rect(x: f64, y: f64, width: f64, height: f64) -> Path {
-    Path::from_points(&vec![
-        Point(x, y),
-        Point(x, y + height),
-        Point(x + width, y + height),
-        Point(x + width, y),
-        Point(x, y),
-    ], true)
+    Path::from_points(
+        &vec![
+            Point(x, y),
+            Point(x, y + height),
+            Point(x + width, y + height),
+            Point(x + width, y),
+            Point(x, y),
+        ],
+        true,
+    )
 }
 
 pub fn triangle(size: f64) -> Path {
@@ -17,5 +37,10 @@ pub fn triangle(size: f64) -> Path {
 pub fn ngon(n: usize, size: f64) -> Path {
     assert!(3 <= n);
     let p = Point(0.0, -size);
-    Path::from_points(&(0..=n).map(|i| p.rotate(i as f64 / n as f64 * std::f64::consts::PI * 2.0)).collect::<Vec<_>>(), true)
+    Path::from_points(
+        &(0..=n)
+            .map(|i| p.rotate(i as f64 / n as f64 * std::f64::consts::PI * 2.0))
+            .collect::<Vec<_>>(),
+        true,
+    )
 }
