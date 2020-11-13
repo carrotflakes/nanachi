6a53a03a40c1775f83fbec29325201f384829a8a
carrotflakes <carrotflakes@gmail.com>
2020-11-14 05:49:34 +0900

    fix interpolation bug

--- src/interpolation/mod.rs
+++ src/interpolation/mod.rs
@@ -20,8 +20,8 @@ impl<P: Pixel, B: Buffer<P>> Interpolation<P, B> for NearestNeighbor {
         let (width, height) = buffer.dimensions();
         buffer
             .get_pixel(
-                x.rem_euclid(width as f64).floor() as u32,
-                y.rem_euclid(height as f64).floor() as u32,
+                (x.rem_euclid(width as f64).floor() as u32).min(width - 1),
+                (y.rem_euclid(height as f64).floor() as u32).min(height - 1),
             )
             .clone()
     }
@@ -34,10 +34,16 @@ pub struct Bilinear;
 impl<P: Pixel, B: Buffer<P>> Interpolation<P, B> for Bilinear {
     fn interpolate(&self, buffer: &B, x: f64, y: f64) -> P {
         let (width, height) = buffer.dimensions();
-        let x = x.rem_euclid(width as f64);
-        let y = y.rem_euclid(height as f64);
+        let mut x = x.rem_euclid(width as f64);
+        let mut y = y.rem_euclid(height as f64);
         let x2 = (x as u32 + 1) % width;
         let y2 = (y as u32 + 1) % height;
+        if x as u32 == width {
+            x =  width as f64 - 0.01;
+        }
+        if y as u32 == height {
+            y =  height as f64 - 0.01;
+        }
         buffer
             .get_pixel(x as u32, y as u32)
             .lerp(buffer.get_pixel(x2, y as u32), x.fract())
