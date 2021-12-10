d7faa1af873dbf935d0f03c59c9ba72d52d8feff
carrotflakes <carrotflakes@gmail.com>
2021-12-10 22:41:56 +0900

    fast_gauss_blur.rs

--- examples/fast_gauss_blur.rs
+++ examples/fast_gauss_blur.rs
@@ -1,31 +1,51 @@
 use nanachi::buffer::{Buffer, GenericBuffer};
 use nanachi::pixel::{Arithmetic, Pixel, Rgba};
 
+pub enum Extrapolation<P: Pixel + Arithmetic> {
+    ExtendEdge,
+    Constant(P),
+}
+
 pub fn gauss_blur<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(
     buf: &mut B,
     tmp: &mut C,
     radius: f64,
+    extrapolation: &Extrapolation<P>,
 ) {
     let bxs = boxes_for_gauss(radius, 3);
-    box_blur(buf, tmp, (bxs[0] - 1) / 2);
-    box_blur(buf, tmp, (bxs[1] - 1) / 2);
-    box_blur(buf, tmp, (bxs[2] - 1) / 2);
+    box_blur(buf, tmp, (bxs[0] - 1) / 2, extrapolation);
+    box_blur(buf, tmp, (bxs[1] - 1) / 2, extrapolation);
+    box_blur(buf, tmp, (bxs[2] - 1) / 2, extrapolation);
 }
 
-fn box_blur<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(buf: &mut B, tmp: &mut C, r: i32) {
+fn box_blur<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(
+    buf: &mut B,
+    tmp: &mut C,
+    r: i32,
+    extrapolation: &Extrapolation<P>,
+) {
     if r <= 0 {
         return;
     }
-    box_blur_h(buf, tmp, r);
-    box_blur_t(tmp, buf, r);
+    box_blur_h(buf, tmp, r, extrapolation);
+    box_blur_t(tmp, buf, r, extrapolation);
 }
 
-fn box_blur_h<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(src: &mut B, dst: &mut C, r: i32) {
+fn box_blur_h<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(
+    src: &mut B,
+    dst: &mut C,
+    r: i32,
+    extrapolation: &Extrapolation<P>,
+) {
     let (w, h) = src.dimensions();
     let iarr = 1.0 / (r + r + 1) as f32;
     for i in 0..h {
-        let fv = src.get_pixel(0, i).clone();
-        let lv = src.get_pixel(w - 1, i).clone();
+        let (fv, lv) = match extrapolation {
+            Extrapolation::ExtendEdge => {
+                (src.get_pixel(0, i).clone(), src.get_pixel(w - 1, i).clone())
+            }
+            Extrapolation::Constant(p) => (p.clone(), p.clone()),
+        };
         let mut val = fv.clone() * (r + 1) as f32;
         for j in 0..r {
             val = val + src.get_pixel(j as u32, i as u32).clone();
@@ -46,12 +66,21 @@ fn box_blur_h<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(src: &mut B, ds
     }
 }
 
-fn box_blur_t<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(src: &mut B, dst: &mut C, r: i32) {
+fn box_blur_t<P: Pixel + Arithmetic, B: Buffer<P>, C: Buffer<P>>(
+    src: &mut B,
+    dst: &mut C,
+    r: i32,
+    extrapolation: &Extrapolation<P>,
+) {
     let (w, h) = src.dimensions();
     let iarr = 1.0 / (r + r + 1) as f32;
     for i in 0..w {
-        let fv = src.get_pixel(i, 0).clone();
-        let lv = src.get_pixel(i, h - r as u32).clone();
+        let (fv, lv) = match extrapolation {
+            Extrapolation::ExtendEdge => {
+                (src.get_pixel(i, 0).clone(), src.get_pixel(i, h - 1).clone())
+            }
+            Extrapolation::Constant(p) => (p.clone(), p.clone()),
+        };
         let mut val = fv.clone() * (r + 1) as f32;
         for j in 0..r {
             val = val + src.get_pixel(i as u32, j as u32).clone();
@@ -113,7 +142,7 @@ fn main() {
 
     let mut tmp = GenericBuffer::from_pixel(width, height, Rgba::zero());
     let t = std::time::Instant::now();
-    gauss_blur(&mut img, &mut tmp, 10.0);
+    gauss_blur(&mut img, &mut tmp, 10.0, &Extrapolation::ExtendEdge);
     // box_blur(&mut img, &mut tmp, 10);
     dbg!(t.elapsed());
     let img: image::RgbaImage = (&img).into();
